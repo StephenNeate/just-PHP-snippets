9168e0a2365614fcb89ab1562ac2b716faa28c61
Stephen <noosalife@gmail.com>
2013-05-22 16:42:05 +1000

    Update printThis.php
    refactored some of the examples

--- output/printThis.php
+++ output/printThis.php
@@ -3,9 +3,9 @@
 // printThis - basic output of data, great for easily seeing what you inputs are
 // $input is primary input, $echo dictates an echo or return value, $title outputs a title if present
 function pt($input,$echo=1,$title=''){
-  ob_start();
-		if( strlen($title) ){
-			print '<p><strong>'.$title.'</strong></p>';
+	ob_start();
+		if( strlen($title) > 0 ){
+			echo '<p><strong>'.$title.'</strong></p>';
 		}
 		echo '<pre>';
 		if(is_array($input)){ 
@@ -19,12 +19,14 @@ function pt($input,$echo=1,$title=''){
 	$returnThis = ob_get_contents();
 	ob_end_clean();
 	
-	if( $echo == 1 ){
+	if( $echo === 1 ){
 		print $returnThis;
 	}else{
 		return $returnThis;
 	}
 }
+
+// the below is just some examples of this function so that you can easily copy/paste to view
 
 echo '<h1>Just the example data and output</h1>';
 
@@ -36,6 +38,6 @@ $json = '[{"value01":10,"value02":"3"},{"value01":11,"value02":"4"}]';
 
 pt($a1);
 pt($a2);
-pt($a3);
-pt($textVal);
-pt($json);
+pt($a3,1,'Multi Dimensional Array');
+echo pt($textVal,0,'Used the return option here for fun');
+pt($json,1,'Some JSON output');
